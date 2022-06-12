e059099b081e618649249f01093fd1535ec3fdb1
Deennn <abideenlawal70@gmail.com>
2022-06-12 15:14:29 +0100

    feedback components

--- src/components/FeedbackStats.jsx
+++ src/components/FeedbackStats.jsx
@@ -6,7 +6,7 @@ function FeedbackStats({feedback}) {
         return acc + curr.rating
     }, 0)/ feedback.length
 
-    average = average.toFixed(1);
+    average = average.toFixed(1).replace(/[.,]0$/, '');
    
   return (
     <div className='feedback-stats'>
